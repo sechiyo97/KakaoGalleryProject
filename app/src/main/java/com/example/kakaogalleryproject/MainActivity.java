f107fa42e5c4de4e566c4925fba7f8576ecbafbb
sechiyo97 <sechiyo97@gmail.com>
2020-07-01 04:25:11 +0900

    AsyncTask to RxJava

--- app/src/main/java/com/example/kakaogalleryproject/MainActivity.java
+++ app/src/main/java/com/example/kakaogalleryproject/MainActivity.java
@@ -15,121 +15,58 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
 public class MainActivity extends AppCompatActivity {
-    protected ArrayList<String> imageList = new ArrayList<String>();
-    private RecyclerView recyclerView;
+    protected ArrayList<String> imageList = new ArrayList<>();
     private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager layoutManager;
-  //  private Handler mHandler; // code piece which was first written for direct threading
-   // private Message message;// code piece which was first written for direct threading
     private ProgressBar mProgress;
-    private DownloadTask downloadTask;
-    //private int START_LOADING = 1111;// code piece which was first written for direct threading
-   // private int END_LOADING = 2222;// code piece which was first written for direct threading
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // code piece which was first written for direct threading
-       /* mHandler = new Handler(){
-            @Override
-            public void handleMessage(Message msg){
-                if (msg.arg1 == START_LOADING) startLoading();
-                if (msg.arg1 == END_LOADING) endLoading();
-            }
-        }; */
-
         mProgress = findViewById(R.id.progress_bar);
 
-        recyclerView = findViewById(R.id.recycler_view);
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setHasFixedSize(true);
 
         // use a staggerd grid layout manager
-        layoutManager = new GridLayoutManager(this, 3);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this, 3);
         recyclerView.setLayoutManager(layoutManager);
 
         // adapter
         mAdapter = new ImageAdapter(imageList);
         recyclerView.setAdapter(mAdapter);
-
-        downloadTask = new DownloadTask();
-        downloadTask.execute();
-
-        //loadImages(); // code piece which was first written for direct threading
+        DownloadTask();
     }
-    // code piece which was first written for direct threading
-    /*
-    public void startLoading(){
+    Disposable downloadtask;
+    void DownloadTask() {
+        // onPreExecute
         mProgress.setVisibility(View.VISIBLE);
-    }
-    public void endLoading(){
-        mAdapter.notifyDataSetChanged(); // dataset changed
-        mProgress.setVisibility(View.GONE);
-    }*/
-    private class DownloadTask extends AsyncTask<Object, String, ArrayList<String>> {
-        @Override
-        protected void onPreExecute(){
-            mProgress.setVisibility(View.VISIBLE);
-        }
-        @Override
-        protected ArrayList<String> doInBackground(Object... params){
-            String url = "https://www.gettyimagesgallery.com/collection/sasha";
+
+        downloadtask = Observable.fromCallable(() -> {
+
+            // onBackground
+            Document doc = Jsoup.connect("https://www.gettyimagesgallery.com/collection/sasha").timeout(3000).get();
             String imageSelector = ".jq-lazy";
-            Document doc = null;
-            try {
-                doc = Jsoup.connect(url).get(); // get doc
-                Elements images = doc.select(imageSelector); // get images
-                for (int i=0;i<images.size();i++) imageList.add(images.get(i).attributes().get("data-src"));
-            } catch (IOException e) {
-                System.out.println(e.getMessage());
-            }
-            return imageList;
-        }
-        @Override
-        protected void onPostExecute(ArrayList<String> imageList){
-            mAdapter.notifyDataSetChanged(); // dataset changed
-            mProgress.setVisibility(View.GONE);
-        }
-    }
+            Elements images = doc.select(imageSelector); // get images
+            for (int i=0;i<images.size();i++) imageList.add(images.get(i).attributes().get("data-src"));
+            return false;
+        })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe((result) -> {
 
-    // below code uses direct threading & messaging to get images
-    /*
-    public void loadImages(){
-        // get images from site
-        parseThread = new Thread(){
-            public void run(){
-                message = mHandler.obtainMessage();
-                message.arg1 = START_LOADING;
-                mHandler.sendMessage(message);
+                    // onPostExecute
+                    mAdapter.notifyDataSetChanged(); // dataset changed
+                    mProgress.setVisibility(View.GONE);
 
-                String url = "https://www.gettyimagesgallery.com/collection/sasha";
-                String imageSelector = ".jq-lazy";
-                Document doc = null;
-                try {
-                    doc = Jsoup.connect(url).get(); // get doc
-                } catch (IOException e) {
-                    System.out.println(e.getMessage());
-                }
-                Elements images = doc.select(imageSelector); // get images
-                for (int i=0;i<images.size();i++)  imageList.add(images.get(i).attributes().get("data-src"));
-
-                message = mHandler.obtainMessage();
-                message.arg1 = END_LOADING;
-                mHandler.sendMessage(message);
-            }
-        };
-        parseThread.start();
-
-       // try{parseThread.join(); } catch(InterruptedException e){}
-
-    }*/
-
-    @Override
-    public void onDestroy( ) {
-        if (downloadTask.getStatus() == AsyncTask.Status.RUNNING) downloadTask.cancel(true);
-        super.onDestroy();
-
+                    downloadtask.dispose();
+                });
     }
 }
