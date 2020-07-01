8a0cd5045f8360e883d4d5c31ab072ecc5ab251c
sechiyo97 <sechiyo97@yonsei.ac.kr>
2020-07-01 22:19:01 +0900

    put loading icon also into each viewholder

--- app/src/main/java/com/example/kakaogalleryproject/ImageAdapter.java
+++ app/src/main/java/com/example/kakaogalleryproject/ImageAdapter.java
@@ -1,16 +1,23 @@
 package com.example.kakaogalleryproject;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 
 import java.util.ArrayList;
 
@@ -20,10 +27,12 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.CustomViewHo
 
     class CustomViewHolder extends RecyclerView.ViewHolder {
         ImageView imageView;
+        ProgressBar adapterProgressBar;
 
         CustomViewHolder(View view) {
             super(view);
-            this.imageView = (ImageView) view.findViewById(R.id.recyclerview_image);
+            this.imageView = view.findViewById(R.id.recyclerview_image);
+            this.adapterProgressBar = view.findViewById(R.id.adapter_progress_bar);
             mContext = view.getContext();
         }
     }
@@ -43,10 +52,23 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.CustomViewHo
 
     @Override
     public void onBindViewHolder(final @NonNull ImageAdapter.CustomViewHolder viewholder, final int position) {
+        viewholder.adapterProgressBar.setVisibility(View.VISIBLE);
         Glide.with(mContext)
                 .load(imageList.get(position).getHref())
                 .transition(DrawableTransitionOptions.withCrossFade())
                 .centerCrop()
+                .listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                        viewholder.adapterProgressBar.setVisibility(View.GONE);
+                        return false;
+                    }
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                        viewholder.adapterProgressBar.setVisibility(View.GONE);
+                        return false;
+                    }
+                })
                 .into(viewholder.imageView); // thumbnail(0.5f) 뺐음, centerCrop().도 뺐음
     }
 
